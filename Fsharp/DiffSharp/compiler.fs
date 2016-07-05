cb535bcc3ff8310b867f36193b51ea869570539a
Amir Shaikhha <t-amshai@microsoft.com>
2016-07-05 21:52:35 +0100

    The first (non-working) version of C code generator

--- Fsharp/DiffSharp/compiler.fs
+++ Fsharp/DiffSharp/compiler.fs
@@ -22,7 +22,34 @@ let rec prettyprint (e:Expr): string =
         | Patterns.Value(v, tp) -> sprintf "%s" (v.ToString())
         | _ -> sprintf "ERROR[%A]" e
 
+let rec ccodegen (e:Expr): string =
+        match e with
+        | Patterns.Lambda (x, body) -> sprintf "ERROR LAMBDA NOT SUPPORTED!"
+        | Patterns.Call (None, op, elist) -> 
+            match op.Name with
+             | OperatorName opname -> sprintf "%s %s %s" (prettyprint elist.[0]) opname (prettyprint elist.[1]) 
+             | "GetArray" -> sprintf "%s[%s]" (prettyprint elist.[0]) (prettyprint elist.[1])
+             | _ -> sprintf "ERROR CALL %s(%s)" op.Name (String.concat ", " (List.map prettyprint elist))
+        | Patterns.Var(x) -> sprintf "%s" x.Name
+        | Patterns.NewArray(tp, elems) -> sprintf "{%s}" (String.concat ", " (List.map prettyprint elems))
+        | Patterns.Value(v, tp) -> sprintf "%s" (v.ToString())
+        | _ -> sprintf "ERROR[%A]" e
 
+let rec ccodegenType (t: System.Type): string = 
+  if(t.IsArray) then
+   (ccodegenType (t.GetElementType()))  + "*"
+  else if(t.IsGenericParameter) then
+    "number_t"
+  else
+    t.ToString()
+
+let ccodegenTop (e: Expr) (name: string): string =
+  let rec extractLambda exp curInputs = match exp with 
+    | Patterns.Lambda (x, body) -> extractLambda body (List.append curInputs [x])
+    | _ -> (exp, curInputs)
+  let (body, inputs) = extractLambda e []
+  
+  sprintf "%s %s(%s) {\n\treturn %s;\n}" (ccodegenType(body.Type)) name (String.concat ", " (List.map (fun (x: Var) -> ccodegenType(x.Type) + " " + x.Name) inputs)) (ccodegen body)
 
 let compile (moduleName: string) (methodName: string) = 
   let a = System.Reflection.Assembly.GetExecutingAssembly()
@@ -30,4 +57,4 @@ let compile (moduleName: string) (methodName: string) =
   let reflDefnOpt = Microsoft.FSharp.Quotations.Expr.TryGetReflectedDefinition(methodInfo)
   match reflDefnOpt with
    | None -> printfn "%s failed" methodName
-   | Some(e) -> printfn "Pretty Printed code for %s.%s:\n %s" moduleName methodName  (prettyprint e)
+   | Some(e) -> printfn "Pretty Printed code for %s.%s:\n\n%s" moduleName methodName  (ccodegenTop e (moduleName + "_" + methodName))
--- coconut/runtime/fsharp.h
+++ coconut/runtime/fsharp.h
@@ -1,6 +1,6 @@
 #ifndef __FSHARP_CLIB_H__ 
 #define __FSHARP_CLIB_H__ 
 
-
+typedef double number_t;
 
 #endif
