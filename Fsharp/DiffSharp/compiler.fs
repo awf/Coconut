ae085af1e4a286f358703cee61e1b3a9bc24bdbe
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-06 14:11:33 +0100

    C code gen uses malloc for NewArray
    * Also a name conflict problem with fresh variables fixed.

--- Fsharp/DiffSharp/compiler.fs
+++ Fsharp/DiffSharp/compiler.fs
@@ -9,6 +9,7 @@ let (|OperatorName|_|) methodName =
     | "op_Subtraction" -> Some("-")
     | _ -> None
 
+(* Pretty prints the given expression *)
 let rec prettyprint (e:Expr): string =
         match e with
         | Patterns.Lambda (x, body) -> sprintf "\%s. %s" (x.Name) (prettyprint body)
@@ -25,11 +26,12 @@ let rec prettyprint (e:Expr): string =
 
 let mutable variable_counter = 0
 
+(* Generates a unique variable name *)
 let newVar (name: string): string = 
   variable_counter <- variable_counter + 1
   sprintf "%s%d" name variable_counter
 
-
+(* C code generation for a type *)
 let rec ccodegenType (t: System.Type): string = 
   if(t.IsArray) then
    "array_" + (ccodegenType (t.GetElementType()))
@@ -38,6 +40,8 @@ let rec ccodegenType (t: System.Type): string =
   else
     t.ToString()
 
+
+(* C code generation for an expression *)
 let rec ccodegen (e:Expr): string =
         match e with
         | Patterns.Lambda (x, body) -> sprintf "ERROR LAMBDA NOT SUPPORTED!"
@@ -47,25 +51,28 @@ let rec ccodegen (e:Expr): string =
              | "GetArray" -> sprintf "%s[%s]" (prettyprint elist.[0]) (prettyprint elist.[1])
              | _ -> sprintf "ERROR CALL %s(%s)" op.Name (String.concat ", " (List.map prettyprint elist))
         | Patterns.Var(x) -> sprintf "%s" x.Name
-        (*
-        | Patterns.Let(x, Patterns.NewArray(tp, elems), e2) -> 
-          sprintf "{%s};" (String.concat ", " (List.map prettyprint elems))
         | Patterns.NewArray(tp, elems) -> 
           sprintf "ERROR new array without let binding [%A]" e
-        *)
-        | Patterns.NewArray(tp, elems) -> 
-          sprintf "(%s[%d]) {%s}" (ccodegenType tp) (List.length elems) (String.concat ", " (List.map prettyprint elems))
         | Patterns.Value(v, tp) -> sprintf "%s" (v.ToString())
         | _ -> sprintf "ERROR[%A]" e
 
+(* C code generation for a statement in the form of `let var = e` *)
+let ccodegenStatement (var: Var, e: Expr): string = 
+  match e with 
+  | Patterns.NewArray(tp, elems) -> 
+    sprintf "%s %s = malloc(sizeof(%s) * %d);\n\t%s" (ccodegenType var.Type) (var.Name) (ccodegenType tp) (List.length elems) 
+      (String.concat "\n\t" (List.mapi (fun index elem -> sprintf "%s[%d] = %s;" var.Name index (prettyprint elem)) elems))
+  | _ -> 
+    sprintf "%s %s = %s;" (ccodegenType var.Type) (var.Name) (ccodegen e)
 
-let ccodegenTop (e: Expr) (name: string): string =
+(* C code generation for a function *)
+let ccodegenFunction (e: Expr) (name: string): string =
   let rec extractHeader exp curInputs statements = match exp with 
     | Patterns.Lambda (x, body) -> extractHeader body (List.append curInputs [x]) statements
     | Patterns.Let(x, e1, e2) -> extractHeader e2 curInputs (List.append statements [(x, e1)])
     | _ -> (exp, curInputs, statements)
   let (result, inputs, statements) = extractHeader e [] []
-  let statementsCode = (String.concat "\n\t" (List.map (fun (lhs: Var, rhs: Expr) -> sprintf "%s %s = %s;" (ccodegenType lhs.Type) (lhs.Name) (ccodegen rhs)) statements))
+  let statementsCode = (String.concat "\n\t" (List.map ccodegenStatement statements))
   sprintf "%s %s(%s) {\n\t%s\n\treturn %s;\n}" (ccodegenType(result.Type)) name (String.concat ", " (List.map (fun (x: Var) -> ccodegenType(x.Type) + " " + x.Name) inputs)) statementsCode (ccodegen result)
 
 (* Performs a simple kind of ANF conversion for specific statements. *)
@@ -75,7 +82,7 @@ let rec cpreprocess (e: Expr): Expr =
   | Patterns.Value(v, tp) -> e
   | Patterns.Lambda (x, body) -> Expr.Lambda(x, cpreprocess body)
   | Patterns.NewArray(tp, elems) -> 
-    let variable = new Var("x", tp.MakeArrayType())
+    let variable = new Var(newVar "array", tp.MakeArrayType())
     Expr.Let(variable, e, Expr.Var(variable))
   | Patterns.Call (x, op, elist) -> 
     Expr.Call(op, List.map cpreprocess elist)
@@ -91,5 +98,5 @@ let compile (moduleName: string) (methodName: string) =
    | Some(e) -> 
      let preprocessed = cpreprocess e
      printfn "preprocessed code:\n%s\n" (prettyprint preprocessed)
-     let generated = ccodegenTop preprocessed (moduleName + "_" + methodName)
+     let generated = ccodegenFunction preprocessed (moduleName + "_" + methodName)
      printfn "Pretty Printed code for %s.%s:\n\n%s" moduleName methodName generated
