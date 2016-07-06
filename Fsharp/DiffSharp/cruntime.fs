c9d859134a9930a94af72628627f358d44f688f9
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-06 19:23:36 +0100

    The first version of cruntime in fsharp

---
+++ Fsharp/DiffSharp/cruntime.fs
@@ -0,0 +1,32 @@
+﻿module cruntime
+
+type Number = double
+type Vector = double List
+type Matrix = Vector List
+
+type AnyNumeric = 
+(*  | ZeroD of Number
+  | OneD of Vector
+  | TwoD of Matrix
+*)
+  Number
+
+type Environment = Map<string, AnyNumeric>
+
+type Closure<'a, 'b> = {
+  lambda: Environment -> ('a -> 'b);
+  env: Environment
+}
+
+let envRef (env: Environment) (name: string): AnyNumeric = 
+  Map.find name env
+
+let makeEnv (bindings: (string * AnyNumeric) List): Environment = 
+  Map.ofList bindings
+
+let makeClosure<'a, 'b> (lambda: Environment -> 'a -> 'b) (env: Environment): Closure<'a, 'b> = 
+  {lambda = lambda; env = env}
+
+let applyClosure<'a, 'b> (closure: Closure<'a, 'b>) (arg: 'a): 'b = 
+  closure.lambda closure.env arg
+
