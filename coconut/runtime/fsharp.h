3dbf9b378c7bb390d44f83f32398b9c098b35962
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-07 16:00:53 +0100

    C runtime adapted to the vector semantic change in the C code generator

--- coconut/runtime/fsharp.h
+++ coconut/runtime/fsharp.h
@@ -2,6 +2,26 @@
 #define __FSHARP_CLIB_H__ 
 
 typedef double number_t;
-typedef number_t* array_number_t;
+typedef struct array_number_t {
+	number_t* arr;
+	int length;
+} array_number_t;
+
+typedef number_t (*lambda_t)();
+
+typedef void* env_t;
+
+typedef struct closure_t {
+	enum Tag t;
+	lambda_t lam;
+	env_t env;
+} closure_t;
+
+closure_t* make_closure(lambda_t lam, env_t env) {
+	closure_t* c = (closure_t*)malloc(sizeof(closure_t));
+	c->lam = lam;
+	c->env = env;
+	return c;
+}
 
 #endif
