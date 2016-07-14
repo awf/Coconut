e07b3f31e5aff8a651b805a686e672117b303a40
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-14 16:49:18 +0100

    Some fixes for inlining

--- Fsharp/DiffSharp/compiler.fs
+++ Fsharp/DiffSharp/compiler.fs
@@ -418,8 +418,15 @@ let optimize (e: Expr): Expr =
           (Expr.Var(new Var(p.Name, p.ParameterType)), new Var(newVar p.Name, p.ParameterType))
         (*let paramList = List.zip argList (List.map paramVar (List.ofSeq (methodInfo.GetParameters())))*)
         let paramList = List.zip (List.map inliner argList) (List.map (fun x -> Expr.Var(x), "") inputs)
-        printfn "%s: name: %A" methodName (List.zip argList paramList) 
-        body.Substitute(fun v -> Option.map (fun (e1, (e2, v1)) -> e1) (List.tryFind (fun (e1, (e2, v1)) -> e2 = Expr.Var(v)) paramList))
+        printfn "%s: name: %A" methodName (List.zip argList paramList)
+        let inlinedBody = body.Substitute(fun v -> Option.map (fun (e1, (e2, v1)) -> e1) (List.tryFind (fun (e1, (e2, v1)) -> e2 = Expr.Var(v)) paramList))
+        match inlinedBody with 
+        | LetN(inputs, letBoundBody) -> 
+          let newInputs = List.map (fun (v: Var, e: Expr) -> (v, e), new Var(newVar (v.Name), v.Type)) inputs
+          LetN(List.map (fun ((_, e), v2) -> (v2, e)) newInputs, 
+            letBoundBody.Substitute(fun v -> Option.map (fun ((v1, e), v2) -> Expr.Var(v2)) (List.tryFind (fun ((v1, _), _) -> v = v1) newInputs)))
+        | _ -> inlinedBody
+        
       | _ -> exp
     | ExprShape.ShapeLambda(i, e) -> Expr.Lambda(i, inliner e)
     | ExprShape.ShapeVar(v) -> Expr.Var(v)
