67bbc1293b1c8b23c1ee17b2da7f04d3d5ff32ac
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-19 14:11:10 +0100

    Precondition and unification added to the rule engine

--- Fsharp/DiffSharp/rules.fs
+++ Fsharp/DiffSharp/rules.fs
@@ -65,33 +65,97 @@ let v_2: Vector = [| s_1 |]
 let m_1: Matrix = [| v_1 |]
 let m_2: Matrix = [| v_1 |]
 *)
+let scalarMetaVars = List.map (fun (x: Expr<Number>) -> x.Raw) [s_1; s_2; s_3]
 let (<==>) (s1: 'a) (s2: 'a):'a = s1
 
-let divide2Mult_2 = <@ ((%s_1 / %s_2) / %s_3) <==> (%s_1 / (%s_2 * %s_3)) @>
+let divide2Mult_2 = 
+  <@ 
+    (%s_1 / %s_2) / %s_3
+    <==> 
+    %s_1 / (%s_2 * %s_3)
+  @>
+
+let distrMult_2 = 
+  <@
+    %s_1 * (%s_2 + %s_3)
+    <==>
+    %s_1 * %s_2 + %s_1 * %s_3
+  @>
+
+let constFold1_2 = 
+  <@ 
+    (%s_1 * 1.)
+    <==>
+    (%s_1)
+  @>
+
+let multDivide_2 = 
+  <@
+    %s_1 * (1. / %s_2)
+    <==>
+    1.
+  @>, 
+  <@ %s_1 = %s_2 @>
+
+let compilePatternWithPreconditionToRule(pat: Expr, precondition: Expr): Rule =
+  let rec extractList(pats: Expr List, exprs: Expr List): (Var * Expr) List Option = 
+    let vars = List.map2 (fun vp ve -> extract(vp, ve)) pats exprs
+    if(List.forall (Option.isSome) vars) then
+      Some(List.concat (List.map (Option.get) vars))
+    else
+      None
+  and extract(p: Expr, e: Expr): (Var * Expr) List Option = 
+    match (p, e) with
+    | (Patterns.Var(v), _) when List.exists (fun x -> x = p) scalarMetaVars -> 
+      Some([v, e])
+    | (Patterns.Call(None, op, pats), Patterns.Call(None, oe, exprs)) when (List.length pats) = (List.length exprs) && op = oe ->
+        extractList(pats, exprs)
+    | (ExprShape.ShapeCombination(op, pats), ExprShape.ShapeCombination(oe, exprs)) when (List.length pats) = (List.length exprs) && op = oe ->
+        extractList(pats, exprs)
+    | (Patterns.Value(v1), Patterns.Value(v2)) when v1 = v2 -> Some([])
+    | _ -> None
+  let unifiedVars = 
+    let rec processPrecondition(pre: Expr): (Var * Var) List =
+      match pre with 
+      | Patterns.Value(v, _) when v.Equals(true) -> []
+      | SpecificCall  <@ (=) @> (None, _, [Patterns.Var(v1) as a; Patterns.Var(v2) as b]) when 
+          (List.exists ((=) a) scalarMetaVars) && (List.exists ((=) b) scalarMetaVars) -> [v1, v2]
+      | SpecificCall  <@ (&&) @> (None, _, [a; b]) -> List.append (processPrecondition a) (processPrecondition b)
+      | _ -> failwith (sprintf "Cannot parse the precondition %A" pre)
+    processPrecondition(precondition)
+  let unification(values: (Var * Expr) List, unifiedVars: (Var * Var) List): (Var * Expr) List Option = 
+    List.fold (fun accOpt (curv, cure) -> 
+        Option.bind (fun acc -> 
+            let sameVariables = curv :: List.collect (fun (v1, v2) -> if(v1 = curv) then [v2] elif (v2 = curv) then [v1] else []) unifiedVars
+            let expressions = List.collect (fun (v, e) -> List.collect (fun v1 -> if(v1 = v) then [e] else []) sameVariables) acc
+            let allAreTheSame = List.forall (fun e -> e = cure) expressions
+            if(allAreTheSame) then
+              Some(List.append acc [curv, cure])
+            else 
+              None
+        ) accOpt
+    ) (Some([])) values
+  fun (exp: Expr) ->
+    let (boundVarsOpt, rhs) = 
+      match pat with 
+      | SpecificCall <@ (<==>) @> (None, _, [p; rhs]) -> 
+        printfn "pattern is %A and rhs is %A" p rhs
+        extract(p, exp), rhs
+      | _ -> failwith "Rewrite patterns should be of the form `lhs <==> rhs`"
+    Option.bind (fun boundVarsInit -> 
+      let unifiedBoundVars = unification(boundVarsInit, unifiedVars)
+      Option.map (fun boundVars ->
+        rhs.Substitute(fun v -> 
+          Option.map (fun (_, e) -> e) 
+            (List.tryFind (fun (v1, _) -> v = v1) boundVars))
+      ) unifiedBoundVars
+    ) boundVarsOpt
+      
 
 let compilePatternToRule(pat: Expr): Rule =
-  fun (exp: Expr) ->
-    let rec extractList(pats: Expr List, exprs: Expr List): (Var * Expr) List Option = 
-      let vars = List.map2 (fun vp ve -> extract(vp, ve)) pats exprs
-      if(List.forall (Option.isSome) vars) then
-        Some(List.concat (List.map (Option.get) vars))
-      else
-        None
-    and extract(p: Expr, e: Expr): (Var * Expr) List Option = 
-      match (p, e) with
-      | (Patterns.Var(v), _) when List.exists (fun x -> x = p) [ s_1.Raw; s_2.Raw; s_3.Raw ] -> 
-        Some([v, e])
-      | (Patterns.Call(None, op, pats), Patterns.Call(None, oe, exprs)) when (List.length pats) = (List.length exprs) && op = oe ->
-          extractList(pats, exprs)
-      | (ExprShape.ShapeCombination(op, pats), ExprShape.ShapeCombination(oe, exprs)) when (List.length pats) = (List.length exprs) && op = oe ->
-          extractList(pats, exprs)
-      | _ -> None
-    let (boundVarsOpt, rhs) = match pat with 
-    | SpecificCall <@ (<==>) @> (None, _, [p; rhs]) -> 
-      printfn "pattern is %A and rhs is %A" p rhs
-      extract(p, exp), rhs
-    | _ -> failwith "Rewrite patterns should be of the form `lhs <==> rhs`"
-    Option.map (fun boundVars -> rhs.Substitute(fun v -> Option.map (fun (_, e) -> e) (List.tryFind (fun (v1, _) -> v = v1) boundVars))) boundVarsOpt
-
+  compilePatternWithPreconditionToRule(pat, <@ true @>)
 
 let divide2Mult_3: Rule = compilePatternToRule divide2Mult_2
+let distrMult_3: Rule = compilePatternToRule distrMult_2
+let constFold1_3: Rule = compilePatternToRule constFold1_2
+let multDivide_3: Rule = compilePatternWithPreconditionToRule multDivide_2
