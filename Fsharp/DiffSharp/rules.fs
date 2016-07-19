10a34bef451dff3009824b1d5ef09c94b01f84b9
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-19 09:36:07 +0100

    The first rewrite rule works with the new engine

--- Fsharp/DiffSharp/rules.fs
+++ Fsharp/DiffSharp/rules.fs
@@ -56,52 +56,41 @@ let multDivide (e: Expr): Expr Option =
      Some(<@@ 1. @@>)
    | _ -> None
 
-let s_1: Number = 1.0
-let s_2: Number = 2.0
-let s_3: Number = 3.0
+let s_1 = Expr.Cast<Number>(Expr.Var(Var.Global("s1", typeof<Number>)))
+let s_2 = Expr.Cast<Number>(Expr.Var(Var.Global("s2", typeof<Number>)))
+let s_3 = Expr.Cast<Number>(Expr.Var(Var.Global("s3", typeof<Number>)))
+(*
 let v_1: Vector = [| s_1 |]
 let v_2: Vector = [| s_1 |]
 let m_1: Matrix = [| v_1 |]
 let m_2: Matrix = [| v_1 |]
+*)
 let (<==>) (s1: 'a) (s2: 'a):'a = s1
 
-let divide2Mult_2 = <@ ((s_1 / s_2) / s_3) <==> (s_1 / (s_2 * s_3)) @>
+let divide2Mult_2 = <@ ((%s_1 / %s_2) / %s_3) <==> (%s_1 / (%s_2 * %s_3)) @>
 
 let compilePatternToRule(pat: Expr): Rule =
   fun (exp: Expr) ->
-    let rec extract(p: Expr, e: Expr): (Var * Expr) List Option = 
+    let rec extractList(pats: Expr List, exprs: Expr List): (Var * Expr) List Option = 
+      let vars = List.map2 (fun vp ve -> extract(vp, ve)) pats exprs
+      if(List.forall (Option.isSome) vars) then
+        Some(List.concat (List.map (Option.get) vars))
+      else
+        None
+    and extract(p: Expr, e: Expr): (Var * Expr) List Option = 
       match (p, e) with
-      | (Patterns.Var(v), _) when List.exists (fun x -> x = p) [ <@@ s_1 @@>; <@@ s_2 @@>; <@@ s_3 @@> ] -> Some([v, e])
-      (*| (ExprShape.ShapeCombination(opraw, pats), ExprShape.ShapeCombination(oeraw, exprs)) when (List.length pats) = (List.length exprs)->
-        (*printf "%s" ((op :?> ExprConstInfo).ToString())*)
-        let foo = match opraw with 
-        | :? (_ * Expr List) as goo ->
-          printf "hi"
-        let ((op, _), (oe, _)) = unbox<_ * _>(opraw), unbox<_ * _>(oeraw)
-        if(op = oe) then 
-          let vars = List.map2 (fun vp ve -> extract(vp, ve)) pats exprs
-          if(List.forall (Option.isSome) vars) then
-            Some(List.concat (List.map (Option.get) vars))
-          else
-            None
-        else
-          None
-      *)
-      | (Patterns.Call(None, op, pats), Patterns.Call(None, oe, exprs)) when (List.length pats) = (List.length exprs)->
-        if(op = oe) then 
-          let vars = List.map2 (fun vp ve -> extract(vp, ve)) pats exprs
-          if(List.forall (Option.isSome) vars) then
-            Some(List.concat (List.map (Option.get) vars))
-          else
-            None
-        else
-          None
+      | (Patterns.Var(v), _) when List.exists (fun x -> x = p) [ s_1.Raw; s_2.Raw; s_3.Raw ] -> 
+        Some([v, e])
+      | (Patterns.Call(None, op, pats), Patterns.Call(None, oe, exprs)) when (List.length pats) = (List.length exprs) && op = oe ->
+          extractList(pats, exprs)
+      | (ExprShape.ShapeCombination(op, pats), ExprShape.ShapeCombination(oe, exprs)) when (List.length pats) = (List.length exprs) && op = oe ->
+          extractList(pats, exprs)
       | _ -> None
     let (boundVarsOpt, rhs) = match pat with 
     | SpecificCall <@ (<==>) @> (None, _, [p; rhs]) -> 
       printfn "pattern is %A and rhs is %A" p rhs
       extract(p, exp), rhs
-    | _ -> failwith "Rewrite patterns should be form `lhs <==> rhs`"
+    | _ -> failwith "Rewrite patterns should be of the form `lhs <==> rhs`"
     Option.map (fun boundVars -> rhs.Substitute(fun v -> Option.map (fun (_, e) -> e) (List.tryFind (fun (v1, _) -> v = v1) boundVars))) boundVarsOpt
 
 
