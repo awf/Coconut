b895d20f6b2423bccd8187b19bf2855aae7c94c4
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-07-29 10:33:59 +0100

    Clean up for C runtime in FSharp

--- Fsharp/DiffSharp/cruntime.fs
+++ Fsharp/DiffSharp/cruntime.fs
@@ -6,58 +6,11 @@ open types
 let envRef (env: Environment) (name: string): AnyNumeric = 
   Map.find name env
 
-let private getIndex (v: AnyNumeric): Index = 
-  match v with 
-  | Idx n -> n 
-  | _ -> failwith (sprintf "Cannot invoke getIndex for %A" v)
+let getAnyNumeric<'a> (v: AnyNumeric): 'a = 
+  failwith "Not implemented!"
 
-let private getNumber (v: AnyNumeric): Number = 
-  match v with 
-  | ZeroD n -> n 
-  | _ -> failwith (sprintf "Cannot invoke getNumber for %A" v)
-
-let private getVector (v: AnyNumeric): Vector = 
-  match v with 
-  | OneD n -> n 
-  | _ -> failwith (sprintf "Cannot invoke getVector for %A" v)
-
-let private getMatrix (v: AnyNumeric): Matrix = 
-  match v with 
-  | TwoD n -> n 
-  | _ -> failwith (sprintf "Cannot invoke getMatrix for %A" v)
-
-let private getMatrix3D (v: AnyNumeric): Matrix3D = 
-  match v with 
-  | ThreeD n -> n 
-  | _ -> failwith (sprintf "Cannot invoke getMatrix3D for %A" v)
-
-let rec getAnyNumeric<'a> (v: AnyNumeric): 'a = 
-  failwith "Not implemented yet!"
-
-and private getFun<'a, 'b> (v: AnyNumeric): 'a -> 'b = 
-  failwith (sprintf "Cannot invoke getFun for %A" v)
-
-let private makeIndex (n: Index): AnyNumeric = 
-  Idx n
-
-let private makeNumber (n: Number): AnyNumeric = 
-  ZeroD n
-
-let private makeVector (n: Vector): AnyNumeric = 
-  OneD n
-
-let private makeMatrix (n: Matrix): AnyNumeric = 
-  TwoD n
-
-let private makeMatrix3D (n: Matrix3D): AnyNumeric = 
-  ThreeD n
-
-let rec makeAnyNumeric<'a> (v: 'a): AnyNumeric = 
-  match v with
-  | _ -> failwith "Not implemented yet!"
-
-and private makeFun<'a, 'b> (* !! *) (f: 'a -> 'b): AnyNumeric = 
-  Fun (fun (x: AnyNumeric) -> makeAnyNumeric(f(getAnyNumeric(x))))
+let makeAnyNumeric<'a> (v: 'a): AnyNumeric = 
+  failwith "Not implemented!"
 
 let makeEnv (bindings: (string * AnyNumeric) List): Environment = 
   Map.ofList bindings
