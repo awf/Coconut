6a0c76cc5115b958f36f15e1c0fd353942e0a87f
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-08-01 18:07:40 +0100

    Added several rules for the mem. mngm. example

--- Fsharp/DiffSharp/Program.fs
+++ Fsharp/DiffSharp/Program.fs
@@ -299,6 +299,7 @@ let main argv =
     compiler.compileModule "programs" ["linalg"] true
     compiler.compileModule "ccodegentests" [] false
     usecases.test1 [||]
+    let comp = ruleengine.compilePatternToRule
     let vecAdd3 = compiler.getMethodExpr "programs" "vector_add3"
     let chains = 
       optimizer.guidedOptimize vecAdd3 
@@ -330,7 +331,20 @@ let main argv =
           rules.lambdaAppToLet, 0;
           rules.letInliner, 0;
           ]
-    printfn "chains: %A" (String.concat "\n*****\n" (List.map ccodegen.prettyprint chains))
+    //printfn "vecAdd3 chains: %A" (String.concat "\n*****\n" (List.map ccodegen.prettyprint chains))
+    let hoistingExample = compiler.getMethodExpr "programs" "hoistingExample"
+    let chains = 
+      optimizer.guidedOptimize hoistingExample 
+        [ rules.vectorSliceToBuild, 0;
+          comp (rules.comAddIndex_exp), 1;
+          comp (rules.assocAddSubIndex_exp), 0;
+          comp (rules.subSameIndex_exp), 0;
+          comp (rules.constFold0Index_exp), 0;
+          //rules.constantFold, 0;
+          comp (rules.vectorBuildToStorage_exp), 0;
+        ]
+    printfn "hoistingExample chains: %A" (String.concat "\n*****\n" (List.map ccodegen.prettyprint chains))
+
     (*test_ba_objective (dir_in + fn) (dir_out + fn) nruns_f nruns_J*)
 #endif
 #if DO_HAND || DO_HAND_COMPLICATED
--- Fsharp/DiffSharp/rules.fs
+++ Fsharp/DiffSharp/rules.fs
@@ -76,11 +76,54 @@ let vectorBuildGet_exp =
     (%FIN) %i
   @>
 
+let comAddIndex_exp = 
+  <@
+    %i + %j
+    <==>
+    %j + %i
+  @>
+
+let assocAddSubIndex_exp = 
+  <@
+    (%i + %j) - %k
+    <==>
+    %i + (%j - %k)
+  @>
+
+let subSameIndex_exp = 
+  <@
+    %i - %i
+    <==>
+    0
+  @>
+
+let constFold0Index_exp = 
+  <@ 
+    %i + 0
+    <==>
+    %i
+  @>
+
+// FIXME doesn't work
+let vectorSliceToBuild_exp = 
+  <@
+    (%V).[(%i)..(%j)]
+    <==>
+    vectorBuild (%j - %i + 1) (fun i -> (%V).[i])
+  @>
+
+let vectorBuildToStorage_exp = 
+  <@
+    vectorBuild (%k) (%FIN)
+    <==>
+    (let s = vectorAlloc (%k) in vectorBuildGivenStorage s (%FIN))
+  @>
+
 let algebraicRulesScalar_exp = [divide2Mult_exp; distrMult_exp; constFold0_exp; constFold1_exp; subSame_exp; multDivide_exp; assocAddSub_exp; assocAddAdd_exp; assocSubSub_exp]
 
 let algebraicRulesScalar: Rule List = List.map compilePatternToRule algebraicRulesScalar_exp
 
-let algebraicRulesVector_exp = [vectorBuildGet_exp]
+let algebraicRulesVector_exp = [vectorBuildGet_exp.Raw; vectorSliceToBuild_exp.Raw]
 
 let algebraicRulesVector: Rule List = List.map compilePatternToRule algebraicRulesVector_exp
 
@@ -105,4 +148,44 @@ let lambdaAppToLet (e: Expr): Expr Option =
   match e with
   | AppN(LambdaN(inputs, body), args) when (List.length inputs) = (List.length args) -> 
       Some(LetN(List.zip inputs args, body))
+  | _ -> None
+
+(*
+// FIXME doesn't work
+let constantFold (e: Expr): Expr Option =
+  match e with
+  | Patterns.Call(None, op, args) ->
+    let staticArgs = 
+      List.collect (fun arg -> 
+        match arg with
+        | Patterns.Value(v, vp) -> [v]
+        | _ -> []
+      ) args
+    
+    if (List.length staticArgs) = (List.length args) then
+      let obj = op.Invoke(null, List.toArray staticArgs)
+      op.
+      Some(Expr.Value(obj))
+    else
+      None
+  | _ -> None
+*)
+
+open types
+
+let vectorSliceToBuild (e: Expr): Expr option =
+  match e with
+  | Patterns.Call (None, op, elist) -> 
+    match op.Name with
+    | "GetArraySlice" when e.Type = typeof<Vector> -> 
+      let args = 
+        List.map (fun x -> 
+          match x with 
+          | Patterns.NewUnionCase(_, [v]) -> v
+          | _ -> x) elist
+      let vec = Expr.Cast<Vector>(args.[0])
+      let s = Expr.Cast<Index>(args.[1])
+      let e = Expr.Cast<Index>(args.[2])
+      Some(<@ vectorBuild (%e - %s + 1) (fun i -> (%vec).[i]) @>.Raw)
+    | _ -> None
   | _ -> None
