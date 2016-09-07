8dbd00994bb28aac0e05c216a6cf8776a9a628f3
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-09-07 22:47:44 +0100

    Added 4 rewrite rules to old-style rules

--- Fsharp/CoconutCompiler/rules_old.fs
+++ Fsharp/CoconutCompiler/rules_old.fs
@@ -12,11 +12,25 @@ let letInliner_old (e: Expr): Expr Option =
   | _ -> None
 
 // c.f. "Compiling with Continuations, Continued", Andrew Kennedy, ICFP'07
-let letCommutingConversion_old (e: Expr): Expr Option = 
-  match e with 
-  | Patterns.Let(v, Patterns.Let(v2, e1, e2), e3) -> 
-    Some(Expr.Let(v2, e1, Expr.Let(v, e2, e3)))
-  | _ -> None
+let letCommutingConversion_old = 
+  let f = (fun (e: Expr) ->
+    match e with 
+    | Patterns.Let(v, Patterns.Let(v2, e1, e2), e3) -> 
+      [Expr.Let(v2, e1, Expr.Let(v, e2, e3))]
+    | _ -> [])
+  f, "lcc_old"
+
+let letNormalization_old = 
+  let f = (fun (e: Expr) ->
+    match e with 
+    | Patterns.Let(v, Patterns.Var(x), e1) -> 
+      [e1.Substitute(fun v2 -> if v2 = v then Some(Expr.Var(x)) else None)]
+    | Patterns.Let(v, (Patterns.Value(_, tp) as vExpr), e1) -> 
+      [e1.Substitute(fun v2 -> if v2 = v then Some(vExpr) else None)]
+    | Patterns.Let(v, (Patterns.Lambda(_, _) as vExpr), e1) -> 
+      [e1.Substitute(fun v2 -> if v2 = v then Some(vExpr) else None)]
+    | _ -> [])
+  f, "ln_old"
 
 let letReorder_old (e: Expr): Expr Option = 
   match e with 
@@ -32,44 +46,85 @@ let letMerging_old (e: Expr): Expr option =
     Some(Expr.Let(x, e1, e3.Substitute(fun v -> if v = y then Some(Expr.Var(x)) else None)))
   | _ -> None
 
-let letVectorBuildLength_old (e: Expr): Expr option =
-  match e with
-  | Patterns.Let (x, (DerivedPatterns.SpecificCall <@ build @> (_, _, [size; f]) as buildExpr), e2) -> 
-    let rec findLength(exp: Expr): (Expr * (Expr -> Expr)) option = 
-      match exp with
-      | Patterns.PropertyGet(Some(arr), prop, []) when prop.Name = "Length" && arr = Expr.Var(x) -> 
-        Some(size, fun i -> i)
-      | ExprShape.ShapeLambda(input, body) ->
-        findLength body
-        |> Option.map (fun (l, f) -> l, fun i -> Expr.Lambda(input, f(i)))
-      | ExprShape.ShapeVar(v) -> None
-      | ExprShape.ShapeCombination(op, args) ->
-        let transformedArg = 
-          args 
-          |> List.mapi (fun idx arg ->
-               findLength(arg)
-               |> Option.map (fun (e, f) -> idx, e, f)
+let letVectorBuildLength_old = 
+  (fun e ->
+    match e with
+    | Patterns.Let (x, (DerivedPatterns.SpecificCall <@ build @> (_, _, [size; f]) as buildExpr), e2) -> 
+      let rec findLength(exp: Expr): (Expr * (Expr -> Expr)) option = 
+        match exp with
+        | ArrayLength(arr) when arr = Expr.Var(x) -> 
+          Some(size, fun i -> i)
+        | ExprShape.ShapeLambda(input, body) ->
+          findLength body
+          |> Option.map (fun (l, f) -> l, fun i -> Expr.Lambda(input, f(i)))
+        | ExprShape.ShapeVar(v) -> None
+        | ExprShape.ShapeCombination(op, args) ->
+          let transformedArg = 
+            args 
+            |> List.mapi (fun idx arg ->
+                 findLength(arg)
+                 |> Option.map (fun (e, f) -> idx, e, f)
+               )
+            |> List.tryPick id
+          transformedArg 
+          |> Option.map (fun (idx, e, f) ->         
+               e, (fun i -> 
+                    let transformedArgs = 
+                      args
+                      |> List.mapi (fun idx2 arg ->
+                           if(idx2 = idx) then
+                             f(i)
+                           else
+                             arg
+                         )
+                    ExprShape.RebuildShapeCombination(op, transformedArgs)
+                  )
              )
-          |> List.tryPick id
-        transformedArg 
-        |> Option.map (fun (idx, e, f) ->         
-             e, (fun i -> 
-                  let transformedArgs = 
-                    args
-                    |> List.mapi (fun idx2 arg ->
-                         if(idx2 = idx) then
-                           f(i)
-                         else
-                           arg
-                       )
-                  ExprShape.RebuildShapeCombination(op, transformedArgs)
-                )
-           )
-    findLength(e2)
-    |> Option.map (fun (te, tf) ->
-         Expr.Let (x, buildExpr, tf(te))
-       )
-  | _ -> None
+      findLength(e2)
+      |> Option.map (fun (te, tf) ->
+           Expr.Let (x, buildExpr, tf(te))
+         ) |> Option.toList
+    | _ -> []), "letBuildLength"
+
+let letVectorBuildGet_old = 
+  (fun e ->
+    match e with
+    | Patterns.Let (x, (DerivedPatterns.SpecificCall <@ build @> (_, _, [size; f]) as buildExpr), e2) -> 
+      let rec findGet(exp: Expr): (Expr * (Expr -> Expr)) option = 
+        match exp with
+        | ArrayGet(arr, idx) when arr = Expr.Var(x) -> 
+          Some(Expr.Application(f, idx), fun i -> i)
+        | ExprShape.ShapeLambda(input, body) ->
+          findGet body
+          |> Option.map (fun (l, f) -> l, fun i -> Expr.Lambda(input, f(i)))
+        | ExprShape.ShapeVar(v) -> None
+        | ExprShape.ShapeCombination(op, args) ->
+          let transformedArg = 
+            args 
+            |> List.mapi (fun idx arg ->
+                 findGet arg
+                 |> Option.map (fun (e, f) -> idx, e, f)
+               )
+            |> List.tryPick id
+          transformedArg 
+          |> Option.map (fun (idx, e, f) ->         
+               e, (fun i -> 
+                    let transformedArgs = 
+                      args
+                      |> List.mapi (fun idx2 arg ->
+                           if(idx2 = idx) then
+                             f(i)
+                           else
+                             arg
+                         )
+                    ExprShape.RebuildShapeCombination(op, transformedArgs)
+                  )
+             )
+      findGet e2
+      |> Option.map (fun (te, tf) ->
+           Expr.Let (x, buildExpr, tf(te))
+         ) |> Option.toList
+    | _ -> []), "letBuildGet"
 
 let allocToCPS_old (e: Expr): Expr option = 
   match e with
@@ -77,3 +132,11 @@ let allocToCPS_old (e: Expr): Expr option =
     let op = assembly.GetType("corelang").GetMethod("vectorAllocCPS").MakeGenericMethod(e2.Type)
     Some(Expr.Call(op, [s; Expr.Lambda(x, e2)]))
   | _ -> None
+
+let dce_old = 
+  (fun e ->
+    match e with
+    | Patterns.Let(x, e1, e2) when not (e2.GetFreeVars() |> Seq.exists (fun x2 -> x2 = x)) ->
+      [e2]
+    | _ -> []
+  ), "dce_old"
