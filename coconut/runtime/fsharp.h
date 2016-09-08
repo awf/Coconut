22edd78fb6a3709fde96ae2a640a6469854b75cb
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-09-08 14:34:22 +0100

    Modularized C runtime

--- coconut/runtime/fsharp.h
+++ coconut/runtime/fsharp.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "types.h"
 
 #define VECTOR_HEADER_BYTES (sizeof(int) * 2)
 #define VECTOR_ALL_BYTES(rows) ((rows) * sizeof(number_t) + VECTOR_HEADER_BYTES)
@@ -12,58 +13,6 @@
 #define STG_OFFSET(stg, offset) (storage_t)(((char*)(stg)) + (offset))
 
 // extern int closure_mem = 0;
-
-typedef int index_t;
-typedef double number_t;
-typedef struct array_number_t_struct {
-	number_t* arr;
-	int length;
-} array_number_t_struct;
-typedef array_number_t_struct* array_number_t;
-typedef struct array_array_number_t {
-	array_number_t* arr;
-	int length;
-}* array_array_number_t;
-typedef struct array_array_array_number_t {
-	array_array_number_t* arr;
-	int length;
-}* array_array_array_number_t;
-
-typedef int card_t;
-typedef struct vector_shape_t {
-  card_t elem;
-  card_t card;
-} vector_shape_t;
-typedef struct matrix_shape_t {
-  vector_shape_t elem;
-  card_t card;
-} matrix_shape_t;
-typedef struct matrix3d_shape_t {
-  matrix_shape_t elem;
-  card_t card;
-} matrix3d_shape_t;
-
-typedef union value_t {
-	card_t card_t_value;
-	vector_shape_t vector_shape_t_value;
-	matrix_shape_t matrix_shape_t_value;
-	matrix3d_shape_t matrix3d_shape_t_value;
-	number_t number_t_value;
-	array_number_t array_number_t_value;
-	array_array_number_t array_array_number_t_value;
-	array_array_array_number_t array_array_array_number_t_value;
-} value_t;
-
-
-typedef void* storage_t;
-
-typedef char* string_t;
-
-typedef value_t (*lambda_t)();
-
-typedef void* env_t;
-
-typedef int empty_env_t;
 
 storage_t empty_storage = (void*)0;
 
---
+++ coconut/runtime/types.h
@@ -0,0 +1,55 @@
+#pragma once
+
+typedef int index_t;
+typedef double number_t;
+typedef struct array_number_t_struct {
+	number_t* arr;
+	int length;
+} array_number_t_struct;
+typedef array_number_t_struct* array_number_t;
+typedef struct array_array_number_t {
+	array_number_t* arr;
+	int length;
+}* array_array_number_t;
+typedef struct array_array_array_number_t {
+	array_array_number_t* arr;
+	int length;
+}* array_array_array_number_t;
+
+typedef int card_t;
+typedef struct vector_shape_t {
+  card_t elem;
+  card_t card;
+} vector_shape_t;
+typedef struct matrix_shape_t {
+  vector_shape_t elem;
+  card_t card;
+} matrix_shape_t;
+typedef struct matrix3d_shape_t {
+  matrix_shape_t elem;
+  card_t card;
+} matrix3d_shape_t;
+
+typedef union value_t {
+	card_t card_t_value;
+	vector_shape_t vector_shape_t_value;
+	matrix_shape_t matrix_shape_t_value;
+	matrix3d_shape_t matrix3d_shape_t_value;
+	number_t number_t_value;
+	array_number_t array_number_t_value;
+	array_array_number_t array_array_number_t_value;
+	array_array_array_number_t array_array_array_number_t_value;
+} value_t;
+
+
+typedef void* storage_t;
+
+typedef char* string_t;
+
+typedef value_t (*lambda_t)();
+
+typedef void* env_t;
+
+typedef int empty_env_t;
+
+typedef unsigned long long int memory_size_t;
