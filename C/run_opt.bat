ee3303298eb04569a080cf69325a9ae8be5c394d
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-09-14 17:01:52 +0100

    Changed folder structure of results and C runtime

--- C/run_opt.bat
+++ C/run_opt.bat
@@ -3,5 +3,5 @@ setLocal EnableDelayedExpansion
 
 for /L %%i in (0,1,10) do (
   call echo "Running upto Level %%i"
-  call ba_opt_%%i.exe "..\data\ba_instances\ba14.txt" >> results\time_opt.txt
+  call ba_opt_%%i.exe "..\data\ba_instances\ba14.txt" >> ..\results\time_opt.txt
 )
