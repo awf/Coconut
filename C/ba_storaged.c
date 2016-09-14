0e191beef5b658c8448019fc36cfc31903933350
Amir Shaikhha <amir.shaikhha@epfl.ch>
2016-09-14 18:18:31 +0100

    Made C version BA produce consistent output format

--- C/ba_storaged.c
+++ C/ba_storaged.c
@@ -12,8 +12,8 @@ int main(int argc, char** argv)
 	card_t m = (int)(nmp->arr[1]);
 	card_t p = (int)(nmp->arr[2]);
 	TOP_LEVEL_usecases_ba_run_ba_from_file_s(empty_storage, file, n, m, p, 0, n, m, p);
-	float milliseconds = (float)(benchmarked_time) * 1000.0 / CLOCKS_PER_SEC;
-	printf("Profiled Time: %d ms\n", (int)milliseconds);
+	// float milliseconds = (float)(benchmarked_time) * 1000.0 / CLOCKS_PER_SEC;
+	// printf("Profiled Time: %d ms\n", (int)milliseconds);
 	// printf("memory of closure: %d\n", closure_mem);
 	return 0;
 }
