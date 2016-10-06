20139972ee08d516f6d845848c8ad5bd792e654a
Andrew Fitzgibbon <awf@microsoft.com>
2016-10-06 13:58:56 +0100

    add comments, maxiter

--- Fsharp/CoconutCompiler/usecases_lm.fs
+++ Fsharp/CoconutCompiler/usecases_lm.fs
@@ -1,5 +1,12 @@
 ﻿module usecases_lm
 
+(*
+  Levenberg-Marquardt implementation.
+
+  Illustrates a common numerical use case: user-supplied function, its derivatives (should be derived algorithmically), and a little matrix algebra.
+  The natural implementation is tail-recursive, but could of course be written as an iteration
+*)
+
 open corelang
 open linalg
 open types
@@ -10,12 +17,8 @@ open cardinality
 let matrixCholeskySolve(m: Matrix) (v: Vector) : Vector =
   build (rows m) (fun i -> v.[0])
 
-(*
-  Levenberg-Marquardt implementation
-*)
-
-// This implementation recomputes the Jacobian too many times -- pass (Some J) to improve
-let rec minimize_lm_aux (f: Vector -> Vector) (jacobian: Vector -> Matrix) (x: Vector) (lambda: Number) : Vector = 
+// This implementation recomputes the Jacobian too many times -- pass a nullable J to improve
+let rec minimize_lm_aux (f: Vector -> Vector) (jacobian: Vector -> Matrix) (x: Vector) (lambda: Number) (max_iter : int) : Vector = 
   let Fx = f x
   let J = jacobian x
   let JtJ = matrixMult (matrixTranspose J) J
@@ -27,14 +30,15 @@ let rec minimize_lm_aux (f: Vector -> Vector) (jacobian: Vector -> Matrix) (x: V
   let ex' = sqnorm Fx'
   if ex < ex' then
       // Good step, accept, and decrease lambda
-      minimize_lm_aux f jacobian xpdelta (lambda / 10.0) 
+      minimize_lm_aux f jacobian xpdelta (lambda / 10.0) (max_iter - 1)
   else
       // Bad step, reject and increase lambda, terminate if lambda too high
-      if lambda > 1e10 then
+      if lambda > 1e10 || max_iter <= 1 then
         x
       else
-        minimize_lm_aux f jacobian x (lambda * 10.0)
+        minimize_lm_aux f jacobian x (lambda * 10.0) (max_iter - 1)
 
 let minimize_lm (f: Vector -> Vector) (jacobian: Vector -> Matrix) (x0: Vector) : Vector = 
-  let lm_lambda = 1e-3 in 
-  minimize_lm_aux f jacobian x0 lm_lambda
+  let lambda = 1e-3 in 
+  let max_iter = 100 in
+  minimize_lm_aux f jacobian x0 lambda max_iter
