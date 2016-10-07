ad43ec3e05b32b6448294c75a5cd6eb82682d50a
amirsh <amir.shaikhha@epfl.ch>
2016-10-07 20:06:11 +0200

    Added script for running (leaky version of) GMM

--- C/compile.sh
+++ C/compile.sh
@@ -1,3 +1,4 @@
 gcc test.c -o test.exe
 gcc ba.c -O3 -o ba.exe
 gcc ba.c -O3 -DBUMP -o ba_bump.exe
+gcc gmm.c -O3 -o gmm.exe
---
+++ C/gmm.c
@@ -0,0 +1,7 @@
+#include "usecases_gmm.h"
+
+int main(int argc, char** argv)
+{
+	TOP_LEVEL_usecases_gmm_Qtimesv_test();
+	return 0;
+}
