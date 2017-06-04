d6180b8a7c06679636f968806e1d8d476b7f9f00
amirsh <amir.shaikhha@epfl.ch>
2017-06-04 05:39:02 +0200

    Symbolic differentation rules for log, exp, sin, cos, and constant values.

--- CoconutCompiler/rules_d.fs
+++ CoconutCompiler/rules_d.fs
@@ -11,6 +11,18 @@ let D_POSTFIX = "_d"
 
 let add_d        = <@ diff (%a + %b) %dx               <==>   (diff %a %dx) + (diff %b %dx)                @>
 let mult_d       = <@ diff (%a * %b) %dx               <==>   (diff %a %dx) * %b + %a * (diff %b %dx)      @>
+let log_d        = <@ diff (log %a)  %dx               <==>   (diff %a %dx) / %a                           @>
+let exp_d        = <@ diff (exp %a)  %dx               <==>   (diff %a %dx) * (exp %a)                     @>
+let sin_d        = <@ diff (sin %a)  %dx               <==>   (diff %a %dx) * (cos %a)                     @>
+let cos_d        = <@ diff (cos %a)  %dx               <==>   (diff %a %dx) * (sin %a)                     @>
+
+let const_d: Rule = 
+  (fun (e: Expr) ->
+    match e with
+    | DerivedPatterns.SpecificCall <@ diff @> (_, _, [Patterns.Value(v1); Patterns.Var(v2)]) ->
+      [ Expr.Value(0.)  ]
+    | _ -> []
+  ), "const_d"
 
 let var_d: Rule = 
   (fun (e: Expr) ->
@@ -18,7 +30,7 @@ let var_d: Rule =
     | DerivedPatterns.SpecificCall <@ diff @> (_, _, [Patterns.Var(v1); Patterns.Var(v2)]) ->
       let res = 
           if v1 = v2 then 
-            Expr.Value(1.f) 
+            Expr.Value(1.) 
           else if not(v1.Name.EndsWith(D_POSTFIX)) then 
             Expr.Var(new Var(v1.Name + D_POSTFIX, typeof<Number>)) 
           else 
--- CoconutCompiler/symbolicdiff.fs
+++ CoconutCompiler/symbolicdiff.fs
@@ -8,11 +8,21 @@ open phase_based_optimizer
 let symdiff (exp: Expr): Expr = 
   exp
     |> trans [ruleengine.compilePatternToRule <@ add_d @>;
-                 ruleengine.compilePatternToRule <@ mult_d @>; var_d; chain_rule] 
+              ruleengine.compilePatternToRule <@ mult_d @>; 
+              ruleengine.compilePatternToRule <@ log_d @>; 
+              ruleengine.compilePatternToRule <@ exp_d @>; 
+              ruleengine.compilePatternToRule <@ sin_d @>; 
+              ruleengine.compilePatternToRule <@ cos_d @>; 
+              const_d;
+              var_d; 
+              chain_rule] 
 
 
 let test_symdiff () = 
     printfn "symdiff: %A" (symdiff <@ fun (x: double) -> diff (1. * 2.) x @>)
     printfn "symdiff2: %A" (symdiff <@ fun (x: double) -> diff (let v = 1. * 2. in v) x @>)
     printfn "symdiff3: %A" (symdiff <@ fun (x: double) (y: double) (z: double) -> diff (let v = y * z in v) x @>)
+    printfn "symdiff4: %A" (symdiff <@ fun (x: double) (y: double) (z: double) -> diff (let v = exp y in v) x @>)
+    printfn "symdiff5: %A" (symdiff <@ fun (x: double) (y: double) (z: double) -> diff (let v = log z in v) x @>)
+    printfn "symdiff6: %A" (symdiff <@ fun (x: double) (y: double) (z: double) -> diff (let v = sin z in v) x @>)
     ()
