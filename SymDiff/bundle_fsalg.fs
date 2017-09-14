f6d690ed67523912b035ee5955127384aa0a0560
Andrew Fitzgibbon <awf@microsoft.com>
2017-09-14 15:29:52 +0100

    Add FsAlg version of bundle

--- SymDiff/bundle_fsalg.fs
+++ SymDiff/bundle_fsalg.fs
@@ -4,27 +4,28 @@ open NUnit.Framework
 open Swensen.Unquote
 open FsAlg.Generic
 
-type Vec<float> = Vector<float>
-
 /// cross: Vector cross product a x b
-let cross (a: Vec<float>, b:Vec<float>) = Vec(a.[1]*b.[2] - a.[2]*b.[1],
-                                              a.[2]*b.[0] - a.[0]*b.[2],
-                                              a.[0]*b.[1] - a.[1]*b.[0])
+let cross (a: Vector<float>, b:Vector<float>) = vector [a.[1]*b.[2] - a.[2]*b.[1]
+                                                        a.[2]*b.[0] - a.[0]*b.[2]
+                                                        a.[0]*b.[1] - a.[1]*b.[0]]
+(*
+let v0 = ZeroVector<float>
+let tmp1 : Vector<Vector<float>> = [| v0 |]
 
 /// cross': Derivative of cross
-// Note that the result is in Vec<Vec>, not Mat, in order to be defined even in the absence of Mat
-let cross' (a: Vec<float>, b:Vec<float>):Vec<Vec<float>>*Vec<Vec<float>> = 
+// Note that the result is in Vector<Vec>, not Mat, in order to be defined even in the absence of Mat
+let cross' (a: Vector<float>, b:Vector<float>):Vector<Vector<float>>*Vector<Vector<float>> = 
     Mat([|0.0, -b.[2], b.[1]; b.[2], 0.0, -b.[0]; -b.[1], b.[0], 0.0|]).v,
     Mat([|0.0, a.[2], -a.[1]; -a.[2], 0.0, a.[0]; a.[1], -a.[0], 0.0|]).v
 
 
 /// cross_matrix: The matrix [a]_x such that [a]_x * b = cross (a, b)
-let cross_matrix (n: Vec<float>) = Mat([|    0.0, -n.[2],    n.[1]; 
+let cross_matrix (n: Vector<float>) = Mat([|    0.0, -n.[2],    n.[1]; 
                                            n.[2],    0.0,   -n.[0]; 
                                           -n.[1],  n.[0],      0.0 |])
 
 /// cross_matrix': Derivative of cross_matrix
-let cross_matrix' (n: Vec<float>) =     Vec(Mat([|  0.0,    0.0,    0.0;  
+let cross_matrix' (n: Vector<float>) =     Vec(Mat([|  0.0,    0.0,    0.0;  
                                                     0.0,    0.0,   -1.0; 
                                                     0.0,    1.0,    0.0  |]),
 
@@ -49,7 +50,7 @@ let test_cross_matrix () =
 
 
 /// rodrigues_rotate_point: Multiply rotation defined by exponential map of ROT by point X
-let rodrigues_rotate_point (rot: Vec<float>, x: Vec<float>) =
+let rodrigues_rotate_point (rot: Vector<float>, x: Vector<float>) =
     let sqtheta = Vec_sumsq rot
     if abs(sqtheta) < 1e-5 then
       let theta = sqrt sqtheta                 
@@ -70,7 +71,7 @@ let rodrigues_rotate_point (rot: Vec<float>, x: Vec<float>) =
       Vec_add (x, cross (rot, x))
 
 /// rodrigues_rotate_point': Derivative of rodrigues_rotate_point
-let rodrigues_rotate_point' (rot: Vec<float>, x: Vec<float>) : Vec<Vec<float>> * Vec<Vec<float>> =
+let rodrigues_rotate_point' (rot: Vector<float>, x: Vector<float>) : Vector<Vector<float>> * Vector<Vector<float>> =
     let rot' = Vec_eye 3, Vec_seye 3 0.0
     let x' = Vec_seye 3 0.0, Vec_eye 3
     let sqtheta = Vec_sumsq rot                 in let sqtheta'     = Vec_sumsq' rot, Vec_zeros 3
@@ -110,13 +111,13 @@ let test_rodrigues () =
     test <@ cf.cf (fd.diff (fun x -> rodrigues_rotate_point (x,b)) atiny, fst (rodrigues_rotate_point' (atiny,b))) @>
 
 /// radial_distort: Distort 2d point using 2-parameter radial distortion model
-let radial_distort (rad_params: Vec<R>, proj: Vec<R>):Vec<R> =
+let radial_distort (rad_params: Vector<R>, proj: Vector<R>):Vector<R> =
     let rsq = Vec_sumsq proj
     let L = 1. + rad_params.[0] * rsq + rad_params.[1] * rsq * rsq
     Vec_smul (L, proj)
 
 /// radial_distort': Derivative of radial_distort
-let radial_distort' (k: Vec<R>, proj: Vec<R>):Vec<Vec<R>> * Vec<Vec<R>> =
+let radial_distort' (k: Vector<R>, proj: Vector<R>):Vector<Vector<R>> * Vector<Vector<R>> =
     let rsq = Vec_sumsq proj            in let rsq' = Vec_sumsq' proj
     let rsqsq = sqr rsq                 in let rsqsq' = GDot.dot(rsq, sqr' rsq, rsq')
     let k0 = k.[0]                      in let k0' =  get' k 0
@@ -137,26 +138,26 @@ let test_radial_distort' () =
 
  (*
 type Camera<'T> = {
-    rot: Vec<'T>
-    origin: Vec<'T>
-    radial: Vec<'T>
+    rot: Vector<'T>
+    origin: Vector<'T>
+    radial: Vector<'T>
     focal_length: 'T
-    center: Vec<'T>
+    center: Vector<'T>
 }
 *)
 
-let getfrom  (v:Vec<float>) lo n = Vec(v.v.[lo..lo+n-1])
-let getfrom' (v:Vec<float>) lo n = Vec((Vec_eye v.size).v.[lo..lo+n-1])
+let getfrom  (v:Vector<float>) lo n = Vec(v.v.[lo..lo+n-1])
+let getfrom' (v:Vector<float>) lo n = Vec((Vec_eye v.size).v.[lo..lo+n-1])
 
-let get (v:Vec<float>) i = v.[i]
-let get' (v:Vec<float>) i = (Vec_eye v.size).[i]
+let get (v:Vector<float>) i = v.[i]
+let get' (v:Vector<float>) i = (Vec_eye v.size).[i]
 
-let proj (x:Vec<float>) : Vec<float> =
+let proj (x:Vector<float>) : Vector<float> =
     assert (x.size = 3)
     let r = recip x.[2]
     Vec([| r * x.[0]; r * x.[1] |])
 
-let proj' (x:Vec<float>) : Vec<Vec<float>> =
+let proj' (x:Vector<float>) : Vector<Vector<float>> =
     let r = recip x.[2]
     Vec([|Vec([|          r;        0.0 |]);
           Vec([|        0.0;          r |]);
@@ -169,7 +170,7 @@ let test_proj() =
     let x = Vec (0.1,-0.02,1.3)
     test <@ cf.cf (fd.diff proj x, proj' x) @>
 
-let project (cam: Vec<float>, x: Vec<float>):Vec<float> =
+let project (cam: Vector<float>, x: Vector<float>):Vector<float> =
     (* Should be changed to global constant variables *)
     let N_CAM_PARAMS = 11
     let ROT_IDX = 0
@@ -189,7 +190,7 @@ let project (cam: Vec<float>, x: Vec<float>):Vec<float> =
     Vec_add (x0, Vec_smul (focal, distorted))
 
 
-let project' (cam: Vec<float>, x: Vec<float>):Vec<Vec<float>> * Vec<Vec<float>> =
+let project' (cam: Vector<float>, x: Vector<float>):Vector<Vector<float>> * Vector<Vector<float>> =
     (* Should be changed to global constant variables *)
     let N_CAM_PARAMS = 11
     assert (cam.size = N_CAM_PARAMS)
@@ -232,3 +233,4 @@ let reproj_err (cams:Matrix) (x:Matrix) (w:Vector) (obs:Matrix) (feat:Matrix): M
     let range = vectorRange (Card 0) (p .- (Card 1))
     vectorMapToMatrix (fun i -> compute_reproj_err cams.[int obs.[int i].[0]] x.[int obs.[int i].[1]] w.[int i] feat.[int i]) range
       *)
+           *)
