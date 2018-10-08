d33e586f07a63c4312347e02876d0a977fb5c2e5
Tom Ellis <tom.ellis@microsoft.com>
2018-10-08 18:33:45 +0100

    WIP marshalling benchmarks

---
+++ outputs/C/coconut_gmm_toplevel.cpp
@@ -0,0 +1,306 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <cassert>
+
+
+/*#ifdef DO_EIGEN
+#define DO_HAND
+#define DO_HAND_COMPLICATED
+#endif*/
+
+#include "../cpp-common/utils.h"
+#include "../cpp-common/defs.h"
+
+#ifdef DO_GMM
+#include "../cpp-common/gmm.h"
+#include "gmm_d.h"
+#endif
+
+#if defined DO_BA
+#include "../cpp-common/ba.h"
+#include "ba_d.h"
+#endif
+
+#if (defined DO_HAND || defined DO_HAND_COMPLICATED)
+#ifdef DO_EIGEN
+#include "../cpp-common/hand_eigen.h"
+#include "hand_eigen_d.h"
+#else
+#include "../cpp-common/hand_light_matrix.h"
+#endif
+#endif
+
+using std::cout;
+using std::endl;
+using std::string;
+using namespace std::chrono;
+
+// This is instead of writing a header file.  Silly me.
+extern "C" {
+double extract_and_run_gmm(int n,
+                           int k,
+                           int d,
+                           int l_sz,
+                           const double *x,
+                           const double *alphas,
+                           const double *means,
+                           const double *qs,
+                           const double *ls,
+                           double wishart_gamma_dps,
+                           double wishart_m_dps,
+                           double *err);
+}
+
+#ifdef DO_GMM
+void test_gmm(const string& fn_in, const string& fn_out,
+  int nruns_f, int nruns_J, double time_limit, bool replicate_point)
+{
+  int d, k, n;
+  vector<double> alphas, means, icf, x;
+  double err;
+  Wishart wishart;
+
+  // Read instance
+  read_gmm_instance(fn_in + ".txt", &d, &k, &n,
+    alphas, means, icf, x, wishart, replicate_point);
+
+  // We don't actually care about this but we have to pass something to preprocess_qs
+  vector<double> sum_qs(k);
+  vector<double> Qdiags(d*k);
+
+  auto l_sz = d * (d - 1) / 2;
+
+  vector<double> ls(l_sz * k);
+
+  int lsi = 0;
+  int lcfi = 0;
+  for (int ik = 0; ik < k; ik++) {
+    // Skip over the qs
+    lcfi += d;
+    for (int il = 0; il < l_sz; l_sz++) {
+      ls[lsi] = icf[lcfi];
+      lsi++;
+      lcfi++;
+    }
+  }                    
+
+  preprocess_qs(d, k, &icf[0], &sum_qs[0], &Qdiags[0]);
+
+  // Test
+  double tf = timer([&]() {
+      extract_and_run_gmm(n,
+                          k,
+                          d,
+                          l_sz,
+                          x.data(),
+                          alphas.data(),
+                          means.data(),
+                          Qdiags.data(),
+                          ls.data(),
+                          wishart.gamma,
+                          wishart.m,
+                          &err);
+  }, nruns_f, time_limit);
+  cout << "err: " << err << endl;
+
+  string name("coconut");
+  double tJ(1);
+
+  write_times(fn_out + "_times_" + name + ".txt", tf, tJ);
+}
+#endif
+
+#if defined DO_BA
+void compute_ba_J(int n, int m, int p, double *cams, double *X,
+  double *w, int *obs, double *feats, double *reproj_err,
+  double *w_err, BASparseMat& J)
+{
+  J = BASparseMat(n,m,p);
+
+  int n_new_cols = BA_NCAMPARAMS + 3 + 1;
+  vector<double> reproj_err_d(2 * n_new_cols);
+  for (int i = 0; i < p; i++)
+  {
+    memset(reproj_err_d.data(), 0, 2 * n_new_cols*sizeof(double));
+
+    int camIdx = obs[2 * i + 0];
+    int ptIdx = obs[2 * i + 1];
+    computeReprojError_d(
+      &cams[BA_NCAMPARAMS*camIdx], 
+      &X[ptIdx * 3], 
+      w[i], 
+      feats[2 * i + 0], feats[2 * i + 1],
+      &reproj_err[2 * i], 
+      reproj_err_d.data());
+
+    J.insert_reproj_err_block(i, camIdx, ptIdx, reproj_err_d.data());
+  }
+
+  for (int i = 0; i < p; i++)
+  {
+    double w_d = 0;
+    computeZachWeightError_d(w[i], &w_err[i], &w_d);
+
+    J.insert_w_err_block(i, w_d);
+  }
+}
+
+void test_ba(const string& fn_in, const string& fn_out,
+  int nruns_f, int nruns_J, double time_limit)
+{
+  int n, m, p;
+  vector<double> cams, X, w, feats;
+  vector<int> obs;
+
+  read_ba_instance(fn_in + ".txt", n, m, p,
+    cams, X, w, obs, feats);
+
+  vector<double> reproj_err(2 * p);
+  vector<double> w_err(p);
+  BASparseMat J(n,m,p);
+
+  double tf = timer([&]() {
+	  ba_objective(n, m, p, cams.data(), X.data(), w.data(),
+		  obs.data(), feats.data(), reproj_err.data(), w_err.data());
+  }, nruns_f, time_limit);
+
+  double tJ = timer([&]() {
+	  compute_ba_J(n, m, p, cams.data(), X.data(), w.data(), obs.data(),
+		  feats.data(), reproj_err.data(), w_err.data(), J);
+  }, nruns_J, time_limit);
+
+#ifdef DO_EIGEN
+  string name("manual_eigen");
+#else
+  string name("manual");
+#endif
+  write_J_sparse(fn_out + "_J_" + name + ".txt", J);
+  write_times(tf, tJ);
+  write_times(fn_out + "_times_" + name + ".txt", tf, tJ);
+}
+
+#endif
+
+#if defined DO_HAND
+void test_hand(const string& model_dir, const string& fn_in, const string& fn_out,
+  int nruns_f, int nruns_J, double time_limit)
+{
+  vector<double> theta;
+#ifdef DO_EIGEN
+  HandDataEigen data;
+#else
+  HandDataLightMatrix data;
+#endif
+
+  read_hand_instance(model_dir, fn_in + ".txt", &theta, &data);
+
+  vector<double> err(3 * data.correspondences.size());
+  vector<double> J(err.size() * theta.size());
+
+  double tf = timer([&]() {
+	  hand_objective(&theta[0], data, &err[0]);
+  }, nruns_f, time_limit);
+
+  double tJ = timer([&]() {
+	  hand_objective_d(&theta[0], data, &err[0], &J[0]);
+  }, nruns_J, time_limit);
+
+#ifdef DO_EIGEN
+  string name = "manual_eigen";
+#else
+  string name = "manual_light";
+#endif
+
+  write_J(fn_out + "_J_" + name + ".txt", (int)err.size(), (int)theta.size(), &J[0]);
+  //write_times(tf, tJ);
+  write_times(fn_out + "_times_" + name + ".txt", tf, tJ);
+}
+
+#elif defined DO_HAND_COMPLICATED
+void test_hand(const string& model_dir, const string& fn_in, const string& fn_out,
+  int nruns_f, int nruns_J, double time_limit)
+{
+  vector<double> theta, us;
+#ifdef DO_EIGEN
+  HandDataEigen data;
+#else
+  HandDataLightMatrix data;
+#endif
+
+  read_hand_instance(model_dir, fn_in + ".txt", &theta, &data, &us);
+
+  vector<double> err(3 * data.correspondences.size());
+  vector<double> J(err.size() * (2+theta.size()));
+
+  double tf = timer([&]() {
+	  hand_objective(&theta[0], &us[0], data, &err[0]);
+  }, nruns_f, time_limit);
+
+  double tJ = timer([&]() {
+	  hand_objective_d(&theta[0], &us[0], data, &err[0], &J[0]);
+  }, nruns_J, time_limit);
+
+#ifdef DO_EIGEN
+  string name = "manual_eigen";
+#else
+  string name = "manual_light";
+#endif
+
+  write_J(fn_out + "_J_" + name + ".txt", (int)err.size(), 2+(int)theta.size(), &J[0]);
+  //write_times(tf, tJ);
+  write_times(fn_out + "_times_" + name + ".txt", tf, tJ);
+}
+
+#endif
+
+char* defaults[] = {
+	"",
+	"../../../",
+	"../../../tmpp/",
+	"test",
+#ifdef _NDEBUG
+	"100",
+	"100"
+#else
+	"2",
+	"2"
+#endif
+};
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		argc = 6;
+		argv = defaults;
+	}
+	else if (argc < 6) {
+		std::cerr << "usage: Manual dir_in dir_out file_basename nruns_F nruns_J [-rep]\n";
+		return 1;
+	}
+
+	string dir_in(argv[1]);
+	string dir_out(argv[2]);
+	string fn(argv[3]);
+	int nruns_f = std::stoi(string(argv[4]));
+    int nruns_J = std::stoi(string(argv[5]));
+	double time_limit;
+	if (argc >= 7) time_limit = std::stod(string(argv[6]));
+	else time_limit = std::numeric_limits<double>::infinity();
+
+  // read only 1 point and replicate it?
+  bool replicate_point = (argc > 6 && string(argv[6]).compare("-rep") == 0);
+
+#ifdef DO_GMM
+  test_gmm(dir_in + fn, dir_out + fn, nruns_f, nruns_J, time_limit, replicate_point);
+#endif
+
+#if defined DO_BA
+  test_ba(dir_in + fn, dir_out + fn, nruns_f, nruns_J, time_limit);
+#endif
+
+#if defined DO_HAND || defined DO_HAND_COMPLICATED
+  test_hand(dir_in + "model/", dir_in + fn, dir_out + fn, nruns_f, nruns_J, time_limit);
+#endif
+}
---
+++ outputs/C/foo.c
@@ -0,0 +1,101 @@
+#include "usecases_gmm_storaged.h"
+
+// The return array is never freed.  For that we would need to run
+// 
+//     storage_free(stgVar1416, size1443);
+//
+// But that's fine for our purposes because once we're done with the
+// input data our program terminates.
+array_number_t make_a_vector(card_t a_shp, const number_t *input) {
+	card_t size1443 = width_card_t(a_shp);
+	array_number_t stgVar1416 = storage_alloc(size1443);
+	array_number_t macroDef1433 = (array_number_t)stgVar1416;
+
+	macroDef1433->length=3;
+	macroDef1433->arr=(number_t*)(STG_OFFSET(stgVar1416, VECTOR_HEADER_BYTES));
+
+        for (int j = 0; j < a_shp; j++) {
+          macroDef1433->arr[j] = input[j];
+        }
+        
+	array_number_t a_dps = macroDef1433;
+        //	array_print(a_dps);
+
+        return a_dps;
+}
+
+// The return array is never freed.  For that we would need to run
+// 
+//     storage_free(stgVar, size1442);
+//
+// But that's fine for our purposes because once we're done with the
+// input data our program terminates.
+array_array_number_t make_a_matrix(card_t irange, card_t jrange, const number_t *input) {
+	matrix_shape_t mat1_shp = nested_shape_card_t(irange, jrange);
+	card_t size1442 = width_matrix_shape_t(mat1_shp);
+
+	array_number_t stgVar = storage_alloc(size1442);
+	array_array_number_t macroDef = (array_array_number_t)stgVar;
+
+	macroDef->length=3;
+	macroDef->arr=(array_number_t*)(STG_OFFSET(stgVar, VECTOR_HEADER_BYTES));
+
+	int stgVar_offsetVar = 0;
+
+        for (int i = 0; i < irange; i++) {
+          storage_t stgVarInner = STG_OFFSET(stgVar, MATRIX_HEADER_BYTES(jrange) + stgVar_offsetVar);
+          array_number_t macroDef_arr_i = (array_number_t)stgVarInner;
+
+          macroDef_arr_i->length=jrange;
+          macroDef_arr_i->arr=(number_t*)(STG_OFFSET(stgVarInner, VECTOR_HEADER_BYTES));
+	
+          for (int j = 0; j < jrange; j++) {
+            macroDef_arr_i->arr[j] = input[i * jrange + j];
+          }
+
+          macroDef->arr[i] = macroDef_arr_i;
+          stgVar_offsetVar += VECTOR_ALL_BYTES(macroDef->arr[i]->length);
+        }
+
+	array_array_number_t mat1_dps = macroDef;
+	// matrix_print(mat1_dps);
+
+        return mat1_dps;
+}
+
+double extract_and_run_gmm(int n,
+                           int k,
+                           int d,
+                           int l_sz,
+                           const double *x,
+                           const double *alphas,
+                           const double *means,
+                           const double *qs,
+                           const double *ls,
+                           double wishart_gamma_dps,
+                           double wishart_m_dps,
+                           double *err) {
+  array_array_number_t x_dps = make_a_matrix(n, d, x);
+  array_number_t alphas_dps = make_a_vector(k, alphas);
+  array_array_number_t means_dps = make_a_matrix(k, d, means);
+  array_array_number_t qs_dps = make_a_matrix(k, d, qs);
+  array_array_number_t ls_dps = make_a_matrix(k, l_sz, ls);
+  
+  *err = TOP_LEVEL_usecases_gmm_gmm_objective_dps(
+          (storage_t)0,
+          x_dps,
+          alphas_dps,
+          means_dps,
+          qs_dps,
+          ls_dps,
+          wishart_gamma_dps,
+          wishart_m_dps,
+          nested_shape_card_t(n, d),
+          k,
+          nested_shape_card_t(k, d),
+          nested_shape_card_t(k, d),
+          nested_shape_card_t(k, l_sz),
+          1,
+          1);
+}
+
